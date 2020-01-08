bf8a88a0cdbc16a4ab7f09a8f8842e4378c26f3f
Jeremy Shank <jeremyshank@bmss.com>
2020-01-08 17:08:45 -0600

    linked database and gui

--- src/Components/Pages/BMSSCPA.jsx
+++ src/Components/Pages/BMSSCPA.jsx
@@ -1,37 +1,79 @@
 import React, { useEffect, useState } from 'react'
+import BmssQ from '../Queries/BmssQ'
 
 export default function BMSSCPA() {
     const [title] = useState("BMSS CPAs & Business Advisors")
-    let [result, setResult] = useState([])
-
+    let [url, setUrl] = useState("http://localhost:3001/")
+    let [clientName, setName] = useState("")
+    let [clientOffice, setOffice] = useState("*")
+    let [partner, setPartner] = useState("")
     useEffect(() => {
         document.title = title
-    })
-
-    useEffect(() => {
-        fetch("http://localhost:3001/")
-            .then(res => {
-                return res.json()
-            })
-            .then(data => {
-                let results = data.recordsets[0].map(result => {
-                return (
-                        <div>
-                            <h2>{result.StaffName}</h2>
-                            <h3>{result.StaffEMail}</h3>
-                            <h3>{result.StaffStarted}</h3>
-                            <h4>Staff Manager? {result.StaffManager ? "Yes" : "No"}</h4>
-                        </div>
-                    )
-                })
-                setResult(results)
-            })
     })
 
     return (
         <div>
             <h1 className="title">{title}</h1>
-            {result}
+            <table>
+                <thead className="fixed">
+                    <tr>
+                        <th className="client">Client Name
+                        <input value={clientName} style={{ marginLeft: 5 }} type="text" onChange={e => {
+                                let cName = e.target.value
+                                setName(cName)
+                            }} />
+                        </th>
+                        <th className="office">Client Office
+                        <select onChange={(e) => {
+                                let office = e.target.value
+                                setOffice(office)
+                            }
+                            } style={{ marginLeft: 5 }}>
+                                <option value="*">All</option>
+                                <option value="BHM">Birmingham</option>
+                                <option value="HSV">Huntsville</option>
+                                <option value="GAD">Gadsden</option>
+                                <option value="AO">Auburn</option>
+                            </select>
+                        </th>
+                        <th className="partner">Partner
+                            <input value={partner} style={{ marginLeft: 5, marginRight: 5 }} type="text" onChange={e => {
+                                    setPartner(e.target.value)
+                                }} />
+                                <button onClick={() => {
+                                    switch (true) {
+                                        case clientOffice !== "*" && clientName.length === 0 && partner.length === 0:
+                                            setUrl(`http://localhost:3001/office/${clientOffice}`)
+                                            break;
+                                        case clientOffice === "*" && clientName.length !== 0 && partner.length === 0:
+                                            setUrl(`http://localhost:3001/client/${clientName}`)
+                                            break;
+                                        case clientOffice === "*" && clientName.length === 0 && partner.length !== 0:
+                                            setUrl(`http://localhost:3001/partner/${partner}`)
+                                            break;
+                                        case clientOffice !== "*" && clientName.length > 0 && partner.length === 0:
+                                            setUrl(`http://localhost:3001/office/${clientOffice}&/client/${clientName}`);
+                                            break;
+                                        case clientOffice !== "*" && clientName.length === 0 && partner.length > 0:
+                                            setUrl(`http://localhost:3001/office/${clientOffice}&/partner/${partner}`);
+                                            break;
+                                        case clientOffice === "*" && clientName.length > 0 && partner.length > 0:
+                                            setUrl(`http://localhost:3001/client/${clientName}&/partner/${partner}`);
+                                            break;
+                                        case clientOffice !== "*" && clientName.length > 0 && partner.length > 0:
+                                            setUrl(`http://localhost:3001/client/${clientName}&/partner/${partner}&/office/${clientOffice}`);
+                                            break;
+                                        default:
+                                            setUrl("http://localhost:3001/")
+                                            break;
+                                    }
+                                }
+                            }>Go</button>
+                        </th>
+                    </tr>
+                </thead>
+                <BmssQ url={url} />
+            </table>
         </div>
     )
 }
---
+++ src/Components/Queries/BmssQ.jsx
@@ -0,0 +1,52 @@
+import React, {useState, useEffect} from 'react'
+
+export default function BmssQ(props) {
+    let [result, setResult] = useState([])
+
+    useEffect(() => {
+        fetch(props.url)
+            .then(res => {
+                return res.json()
+            })
+            .then(data => {
+                let results = data.recordsets[0].map((result, index) => {
+                    let style = ""
+                    switch (true) {
+                        case index % 2 === 0 && result.ClientStatus === "LOST":
+                            style = "lost-even";
+                            break;
+                        case index % 2 === 0 && result.ClientStatus === "ACTIVE":
+                            style = "active-even";
+                            break;
+                        case index % 2 === 1 && result.ClientStatus === "LOST":
+                            style = "lost-odd";
+                            break;
+                        case index % 2 === 1 && result.ClientStatus === "ACTIVE":
+                            style = "active-odd";
+                            break;
+                        default:
+                            style = "active-odd";
+                    }
+                    return (
+                        <tr className={style} key={result.ContIndex}>
+                            <td className="client">{result.ClientName}</td>
+                            <td className="office">{result.ClientOffice}</td>
+                            <td className="partner">{result.Partner}</td>
+                        </tr>
+                    )
+                }
+                )
+                setResult(results)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+        }, [props.url]
+    )
+
+    return (
+            <tbody>
+                {result}
+            </tbody>
+    )
+}
